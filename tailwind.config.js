4d810e4f54f1b710f989fefee831168e9118d456
iddimsangi <adammsangi@gmail.com>
2023-10-02 16:31:20 +0300

    conf:project environment setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "very-dark-blue-bg": "hsl(207, 26%, 17%)",
+        "very-dark-blue-text": "hsl(200, 15%, 8%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
