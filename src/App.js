89178326b88e20608f0904997d8ccb4f166ba665
iddimsangi <adammsangi@gmail.com>
2023-10-04 11:24:36 +0300

    feat:add svg icon and use tag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import sprite from "./prite.svg";
 function App() {
   return (
     <div className="App">
       <div class="font-nunito">This text uses the Nunito Sans font family.</div>
+      <svg fill="green">
+        <use xlinkHref={`${sprite}#icon-moon`}></use>
+      </svg>
     </div>
   );
 }
