947d84aa8b529c0c3432a5175a214f43bd3f5567
iddimsangi <adammsangi@gmail.com>
2023-10-24 00:34:36 +0300

    feat:Implement country search functionality

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,21 +1,49 @@
+import { useState } from "react";
 import Country from "./Country";
 import TopHeading from "./TopHeading";
 function CountriesList({ countries, selectHandler }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
+  // Filter countries based on the search query
+  const filteredCountries = countries.filter((country) => {
+    return country.name.official
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase());
+  });
+
   return (
     <section>
-      <TopHeading />
+      <TopHeading handleSearch={handleSearch} searchQuery={searchQuery} />
       <div className="min-h-full flex flex-col space-y-5 justify-center items-center p-12 sm:grid sm:grid-cols-2 sm:gap-3 sm:p-4 sm:space-y-0 md:grid md:grid-cols-4 md:gap-4 md:space-y-0 md:p-4 md:max-w-[1440px]">
-        {countries.map((country) => (
-          <Country
-            key={country.name.official}
-            flagImg={country.flags.png}
-            countryName={country.name.official}
-            population={country.population}
-            region={country.region}
-            capital={country.capital}
-            selectHandler={selectHandler}
-          />
-        ))}
+        {searchQuery
+          ? // Display filtered countries when there's a search query
+            filteredCountries.map((country) => (
+              <Country
+                key={country.name.official}
+                flagImg={country.flags.png}
+                countryName={country.name.official}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+                selectHandler={selectHandler}
+              />
+            ))
+          : // Display all countries when there's no search query
+            countries.map((country) => (
+              <Country
+                key={country.name.official}
+                flagImg={country.flags.png}
+                countryName={country.name.official}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+                selectHandler={selectHandler}
+              />
+            ))}
       </div>
     </section>
   );
--- src/components/SearchCountry.js
+++ src/components/SearchCountry.js
@@ -1,5 +1,5 @@
 import sprite from "../sprite.svg";
-function SearchCountry() {
+function SearchCountry({ handleSearch, searchQuery }) {
   return (
     <form
       className="bg-dark-blue flex mt-4 relative items-center w-full md:max-w-md rounded-md shadow-lg"
@@ -16,6 +16,8 @@ function SearchCountry() {
       <input
         type="text"
         id="name"
+        value={searchQuery}
+        onChange={(e) => handleSearch(e.target.value)}
         className="bg-transparent py-5 text-md outline-none pl-24  placeholder:text-white text-very-light-gray w-11/12"
         placeholder="Search for country"
       />
